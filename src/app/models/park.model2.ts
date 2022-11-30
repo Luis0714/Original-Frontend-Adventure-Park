3af134ae62a56162ae099e9ae483cb2d930bb494
juniorecheverri <juniorecheverri9@gmail.com>
2022-11-30 01:23:15 -0500

    listar parques

---
+++ src/app/models/park.model2.ts
@@ -0,0 +1,10 @@
+export class ParkModel{
+    nombre: string = ''
+    direccion: string = ''
+    cantidadVisitas: string = ''
+    logo: string = ''
+    mapa: string = ''
+    slogan: string = ''
+    descripcion: string = ''
+    ciudadId: string = ''
+}
--- src/app/modules/parameters/parks/list-park/list-park.component.ts
+++ src/app/modules/parameters/parks/list-park/list-park.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ApisInfo } from 'src/app/config/apisInfo';
+import { ParkModel } from 'src/app/models/park.model';
+import { ParkService } from 'src/app/services/parameters/park.service';
 
+declare const OpenConfirmModal: any;
 @Component({
   selector: 'app-list-park',
   templateUrl: './list-park.component.html',
@@ -7,9 +11,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListParkComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private parkService: ParkService
+  ) { 
+    
   }
 
+  idToRemove: string = "";
+  recordList: ParkModel[]= [];
+  urlServer = ApisInfo.MS_LOG_URL;
+
+  ngOnInit(): void {
+    this.ListRecords();
+  }
+
+  ListRecords() {
+    this.parkService.getRecorList().subscribe({
+      next: (data) => {
+        this.recordList = data;
+        console.log("parques ",this.recordList)
+        
+      },
+      error: (err) => {
+        alert("Error obteniendo la información")
+      }
+    })
+  }
+
+  ShowRemoveWindow(id: string) {
+    OpenConfirmModal("¿Está seguro que dea elimminar el departamento?")
+    this.idToRemove = id;
+  }
+
+  
+  /**
+   * 
+   * @param id 
+   */
+   RemoveRecord() {
+    this.parkService.removeRecord(this.idToRemove).subscribe({
+      next: (data) => {
+        //this.ListRecords();
+        this.recordList = this.recordList.filter(x => x.id != this.idToRemove);
+        
+      },
+      error: (err) => {
+        alert("Error obteniendo la información")
+      }
+    })
+  }
 }
