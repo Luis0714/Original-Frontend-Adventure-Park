d9830f681aa5744b1f248263aff2ad5bf3764787
juniorecheverri <juniorecheverri9@gmail.com>
2022-11-29 22:40:12 -0500

    errores

--- src/app/modules/parameters/parameters.module.ts
+++ src/app/modules/parameters/parameters.module.ts
@@ -33,12 +33,6 @@ import { EditDepartmentComponent } from './department/edit-department/edit-depar
 import { ListDepartmentComponent } from './department/list-department/list-department.component';
 import { RemoveDepartmentComponent } from './department/remove-department/remove-department.component';
 import { ContactenosComponent } from './contactenos/contactenos/contactenos.component';
-import { CreateCityComponent } from './city/create-city/create-city.component';
-import { EditCityComponent } from './city/edit-city/edit-city.component';
-import { ListCityComponent } from './city/list-city/list-city.component';
-import { RemoveCityComponent } from './city/remove-city/remove-city.component';
-
-
 
 
 @NgModule({
